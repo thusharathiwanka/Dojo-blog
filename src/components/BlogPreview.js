bd9b999fc4a9941020f9ed8f8a3e272b07f4b7b5
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-02-10 18:57:37 +0530

    CREATE: blog delete added

---
+++ src/components/BlogPreview.js
@@ -0,0 +1,22 @@
+const BlogPreview = ({ blogs, handleDelete }) => {
+  return (
+    <div className="blogs-content">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h3>{blog.title}</h3>
+          <p>{blog.content}</p>
+          <p>By {blog.author}</p>
+          <button
+            onClick={() => {
+              handleDelete(blog.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPreview;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogPreview from "./BlogPreview";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -6,14 +7,14 @@ const Home = () => {
       id: 1,
       title: "Apple may be giving up on the iPhone 12 mini",
       content:
-        "To appease iPhone fans, Apple included a smaller version of the phone in its iPhone 12 lineup, the iPhone 12 mini. You would...",
+        "To appease iPhone fans, Apple included a smaller version of the phone in its iPhone 12 lineup...",
       author: "Kevin Raposo",
     },
     {
       id: 2,
       title: "Coumi ANC-860 earbuds are a perfect all-round option",
       content:
-        "Ever since the days of my Sony Walkman, I have always preferred over-heads as opposed to pushing something into my ear...",
+        "Ever since the days of my Sony Walkman, I have always preferred over-heads as opposed to...",
       author: "Sophia Waterfield",
     },
     {
@@ -21,21 +22,20 @@ const Home = () => {
       title:
         "You’ll soon be able to schedule outgoing texts in Google Messages",
       content:
-        "If you’re an Android user, go see if your Google Messages app now has the power to schedule your texts. Google has been...",
+        "If you’re an Android user, go see if your Google Messages app now has the power to...",
       author: "Joe Jones",
     },
   ]);
 
+  const handleDelete = (blogID) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== blogID);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home-content">
       <h2>All Blogs</h2>
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h3>{blog.title}</h3>
-          <p>{blog.content}</p>
-          <p>By {blog.author}</p>
-        </div>
-      ))}
+      <BlogPreview blogs={blogs} handleDelete={handleDelete} />
     </div>
   );
 };
