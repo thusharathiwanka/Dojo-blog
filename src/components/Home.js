1a14ca1b45fb94c77ea6d45ecb0546c3cc3ca404
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-02-10 17:11:16 +0530

    CREATE: blog-preview component styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <h2>All Blogs</h2>;
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      title: "Apple may be giving up on the iPhone 12 mini",
+      content:
+        "To appease iPhone fans, Apple included a smaller version of the phone in its iPhone 12 lineup, the iPhone 12 mini. You would...",
+      author: "Kevin Raposo",
+    },
+    {
+      id: 2,
+      title: "Coumi ANC-860 earbuds are a perfect all-round option",
+      content:
+        "Ever since the days of my Sony Walkman, I have always preferred over-heads as opposed to pushing something into my ear...",
+      author: "Sophia Waterfield",
+    },
+    {
+      id: 3,
+      title:
+        "You’ll soon be able to schedule outgoing texts in Google Messages",
+      content:
+        "If you’re an Android user, go see if your Google Messages app now has the power to schedule your texts. Google has been...",
+      author: "Joe Jones",
+    },
+  ]);
+
+  return (
+    <div className="home-content">
+      <h2>All Blogs</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h3>{blog.title}</h3>
+          <p>{blog.content}</p>
+          <p>By {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
