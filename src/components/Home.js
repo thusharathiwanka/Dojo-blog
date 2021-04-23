a021ebfa61af7f3f554960fbe7a1d7cfe3ea2a34
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-04-23 21:13:37 +0530

    CREATE : fetch error handling added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import BlogPreview from "./BlogPreview";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (blogID) => {
     const newBlogs = blogs.filter((blog) => blog.id !== blogID);
@@ -10,16 +12,42 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
-      .then((blogs) => {
-        setBlogs(blogs);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data");
+          }
+          return response.json();
+        })
+        .then((blogs) => {
+          setBlogs(blogs);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsPending(false);
+        });
+    }, 2000);
   }, []);
+
+  const commonStyles = {
+    textAlign: "center",
+    paddingTop: "10rem",
+  };
+
+  const errorStyles = {
+    textAlign: "center",
+    paddingTop: "10rem",
+    color: "red",
+  };
 
   return (
     <div className="home-content">
       <h2>All Blogs</h2>
+      {error && <p style={errorStyles}>{error}</p>}
+      {isPending && <p style={commonStyles}>Loading...</p>}
       {blogs && <BlogPreview blogs={blogs} handleDelete={handleDelete} />}
     </div>
   );
