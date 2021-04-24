3432e50567e2be6ac4de6497e3948707ea6a11e6
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-04-24 15:29:15 +0530

    CREATE : new blog adding implemented

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBlog = { title, content, author };
+    setIsAdding(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBlog),
+    }).then(() => {
+      setTitle("");
+      setContent("");
+      setAuthor("");
+    });
+  };
+
   return (
-    <form action="" className="new-blog-form">
+    <form className="new-blog-form" onSubmit={handleSubmit}>
       <div className="blog-title">
         <label htmlFor="blog-title">Blog Title</label>
-        <input type="text" name="title" id="blog-title" />
+        <input
+          type="text"
+          name="title"
+          id="blog-title"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="blog-content">
         <label htmlFor="blog-content">Blog Content</label>
@@ -12,13 +43,29 @@ const Form = () => {
           id="blog-content"
           cols="30"
           rows="10"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         ></textarea>
       </div>
       <div className="blog-author">
         <label htmlFor="author-name">Author's Name</label>
-        <input type="text" name="author" id="author-name" />
+        <input
+          type="text"
+          name="author"
+          id="author-name"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
-      <button type="submit">Submit Blog</button>
+      {!isAdding ? (
+        <button type="submit">Submit Blog</button>
+      ) : (
+        <button type="submit" disabled>
+          Submitting
+        </button>
+      )}
     </form>
   );
 };
